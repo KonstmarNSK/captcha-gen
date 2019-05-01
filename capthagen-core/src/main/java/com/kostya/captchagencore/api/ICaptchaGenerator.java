173fd0a528c16397cead359be004c27e1b52e9eb
KonstmarNSK <konstmar@yandex.ru>
2019-05-01 20:59:14 +0700

    initial commit

---
+++ capthagen-core/src/main/java/com/kostya/captchagencore/api/ICaptchaGenerator.java
@@ -0,0 +1,7 @@
+package com.kostya.captchagencore.api;
+
+import java.io.InputStream;
+
+public interface ICaptchaGenerator {
+    InputStream getCaptcha();
+}
---
+++ capthagen-core/src/main/java/com/kostya/captchagencore/impl/CaptchaGeneratorImpl.java
@@ -0,0 +1,11 @@
+package com.kostya.captchagencore.impl;
+
+import com.kostya.captchagencore.api.ICaptchaGenerator;
+
+import java.io.InputStream;
+
+public class CaptchaGeneratorImpl implements ICaptchaGenerator {
+    public InputStream getCaptcha() {
+        throw new UnsupportedOperationException("not implemented yet");
+    }
+}
